88d4ca85a42a5b604c56089ba3bf0ce4e269dcb6
mo <moshpit2241@gmail.com>
2019-05-13 17:17:24 +0800

    change coinmarket api base to usd

--- controllers/curr-converter.controller.js
+++ controllers/curr-converter.controller.js
@@ -1,5 +1,25 @@
+const rp = require('request-promise');
+
 exports.getConversion = (req, res, next) => {
-	res.json({
-		message: 'Successfully accessed endpoint'
-	});
+	const rp = require('request-promise');
+	const requestOptions = {
+		method: 'GET',
+		uri: 'https://api.coinmarketcap.com/v1/ticker/ethereum/',
+		qs: {
+			start: '1',
+			limit: '5000',
+			convert: 'USD'
+		},
+		json: true,
+		gzip: true
+	};
+
+	rp(requestOptions)
+		.then((response) => {
+			console.log('API call response:', response);
+			res.json(response);
+		})
+		.catch((err) => {
+			console.log('API call error:', err.message);
+		});
 };
