9a757340f254c672add2ee68f2b71ad1a64f3ca9
mo <moshpit2241@gmail.com>
2019-05-17 01:10:55 +0800

    sign up progress

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,15 +1,23 @@
 const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.userCreate = (req, res, next) => {
 	console.log(req.file);
-	let user = new User({
-		firstName: req.body.firstName,
-		lastName: req.body.lastName,
-		email: req.body.email,
-		image: req.file.path
-	});
-
-	user.save().then((user) => {
-		res.json(user);
-	});
+	const password = req.body.password;
+	bcrypt
+		.hash(password, 12)
+		.then((hashedPw) => {
+			const user = new User({
+				firstName: req.body.firstName,
+				lastName: req.body.lastName,
+				email: req.body.email,
+				password: hashedPw,
+				image: req.file.path
+			});
+			return user.save();
+		})
+		.then((result) => {
+			res.status(201).json(user);
+		});
 };
--- models/user.model.js
+++ models/user.model.js
@@ -5,6 +5,7 @@ let UserSchema = new Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
 	email: { type: String, required: true },
+	hash_password: { type: String, required: true },
 	image: { type: String, required: true }
 });
 
